9ec110069a923b1a9b658f8f60efaf3e83f26b6a
matheusmartins56 <matheus.martins@dcomp.ufs.br>
2023-04-29 22:42:59 -0300

    feat: Endpoints para buscar orgãos, exames (por orgão) e cadastrar exame

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { MedicController } from '@/controllers/Medic';
 import { SessionController } from '@/controllers/Session'
+import { ExamController } from '@/controllers/Exam';
+import { OrganController } from '@/controllers/Organ';
 
 async function routes(fastify: FastifyInstance) {
   const medicController = new MedicController();
   const sessionController = new SessionController(fastify);
+  const examController = new ExamController()
+  const organController = new OrganController()
 
   fastify.get("/", async (_, __) => {
     return { message: 'Hello World!' }
@@ -15,6 +19,10 @@ async function routes(fastify: FastifyInstance) {
 
   fastify.post("/signin", sessionController.create.bind(sessionController))
   fastify.post("/signup", medicController.create.bind(medicController));
+
+  fastify.get("/organs", organController.index.bind(organController))
+  fastify.get("/exams/:organ", examController.index.bind(examController))
+  fastify.post("/exams", examController.create.bind(examController))
 }
 
 export default routes
